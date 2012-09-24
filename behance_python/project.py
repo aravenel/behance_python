b74a0100f2d8bfcf0f310c8c488cec3050ec2119
Alex Ravenel <ravenel@gmail.com>
2012-09-24 19:46:46 -0400

    Minor documentation.

--- behance_python/project.py
+++ behance_python/project.py
@@ -4,6 +4,7 @@ from behance_python import *
 from requests.exceptions import ConnectionError, HTTPError, Timeout, TooManyRedirects
 
 class Project:
+    """Class representing a Behance project."""
 
     def __init__(self, project_id, auth_key):
         self.project_id = project_id
@@ -13,11 +14,13 @@ class Project:
         self._get_project_details()
 
     def _add_property(self, name, value):
+        """Helper function to dynamically add all the JSON data from API response
+        to the Project object."""
         setattr(self.__class__, name, value)
 
     def _get_project_details(self):
+        """Internal function to call Behance API and parse data."""
         try:
-            #_url = urlparse.urljoin(API_ENDPOINT, PROJECT_ENDPOINT, self.project_id)
             _url = '/'.join(s.strip('/') for s in [API_ENDPOINT, PROJECT_ENDPOINT, self.project_id])
             _url = "%s?api_key=%s" % (_url, self.auth_key)
             _results = requests.get(_url)
