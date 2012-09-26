5aaf075dedc0f485f75bff732888ef6122fe7179
Alex Ravenel <ravenel@gmail.com>
2012-09-25 21:26:57 -0400

    Added packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='behance_python',
+        packages=['behance_python',],
+        install_requires=['requests'],
+        license='MIT',
+        author='Alex Ravenel',
+        author_email='ravenel@gmail.com',
+        version='0.1.0',
+        )
